d9d99a5233fae31406db4e1c45d574086254c5ce
lucas-01-script <lucas.r.freitas6@edu.senai.br>
2026-04-28 09:18:41 -0300

    Add files via upload

---
+++ bibliotecafuncaoes.php
@@ -0,0 +1,6 @@
+<?php
+ // dolarParaReal($valor, $cotacao)
+//euroParaReal($valor, $cotacao)
+// pesoParaReal($valor, $cotacao)
+// libraParaReal($valor, $cotacao)
+// ieneParaReal($valor, $cotacao)
